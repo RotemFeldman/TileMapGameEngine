80f8dc96bfdb4d135fe47755c6911f0494276ad3
RotemFeldman <128305863+RotemFeldman@users.noreply.github.com>
2024-03-04 00:50:31 +0200

    positioning docs and deps
    deps not working? need help

--- Engine/TileObject.cs
+++ Engine/TileObject.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TileMapGameEngine
 {
-    public abstract class TileObject
+    public abstract class TileObject 
     {
        
     }
--- Positioning_TileMapEngine/IPosition.cs
+++ Positioning_TileMapEngine/IPosition.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Positioning_TileMapEngine
 {
+    /// <summary>
+    /// An interface for objects that have a position in two-dimensional space.
+    /// </summary>
     interface IPosition
     {
+        /// <summary>
+        /// Gets or sets the position of the object.
+        /// </summary>
+        /// <remarks>
+        /// The position is represented by a <see cref="Vector2"/> instance.
+        /// </remarks>
         public Vector2 Position { get; set; }
 
+        /// <summary>
+        /// Sets the position of the object to the specified coordinates.
+        /// </summary>
+        /// <param name="position">The new position of the object.</param>
         public void SetPosition(Vector2 position);
     }
 }
